35f9e4d606ed1a084f6af5a1b9750d9e2312cf79
msfrmti-ai <ms.frm.ti@gmail.com>
2025-10-01 10:46:47 +0000

    ggsdf

--- main.tf
+++ main.tf
@@ -12,26 +12,26 @@ provider "azurerm" {
 
 terraform {
   backend "azurerm" {
-    resource_group_name  = "workshop"
-    storage_account_name = "ststorage1234125"
+    resource_group_name  = "rg-10"
+    storage_account_name = "workshop111"
     container_name       = "tfstate"
     key                  = "terraform.tfstate"
   }
 }
 
 resource "azurerm_service_plan" "example" {
-  name                = "example-app-service-plan123123"
+  name                = "user10uut"
   location            = "westeurope"
-  resource_group_name = "workshop"
+  resource_group_name = "rg-10"
   os_type             = "Linux"
   sku_name            = "P0v3"
 }
 
 
 resource "azurerm_linux_web_app" "example" {
-  name                = "example-webapp-123123i95u8fhwfdsewdwsa123"
+  name                = "rtegytyhgijyuokiu"
   location            = "westeurope"
-  resource_group_name = "workshop"
+  resource_group_name = "rg-10"
   service_plan_id     = azurerm_service_plan.example.id
   site_config {}
 }
